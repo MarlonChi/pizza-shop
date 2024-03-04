c42964199a9faaeb8ee86c32cd74253744118eed
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-04 15:46:00 -0300

    feat: dashboard graphs

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,8 @@ import { MonthRevenueCard } from "./month-revenue-card";
 import { MonthOrdersAmountCard } from "./month-orders-amount-card";
 import { DayOrdersAmountCard } from "./day-orders-amount-card";
 import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount";
+import { RevenueChart } from "./revenue-chart";
+import { PopularProductsChart } from "./popular-product-chart";
 
 export const Dashboard = () => {
   return (
@@ -17,6 +19,11 @@ export const Dashboard = () => {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </div>
