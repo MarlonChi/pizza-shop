4b5c12e7846626c9754596df7396b4887186f00b
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-06 17:10:43 -0300

    feat: orders list

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,11 +1,24 @@
 import { ArrowRight, Search, X } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { Button } from "@/components/ui/button";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "./order-status";
 
-export const OrderTableRow = () => {
+interface OrderTableRow {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  };
+}
+
+export const OrderTableRow = ({ order }: OrderTableRow) => {
   return (
     <TableRow>
       <TableCell>
@@ -20,17 +33,24 @@ export const OrderTableRow = () => {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        15235571fsad8412
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
-      <TableCell className="font-medium">Marlon Chiodelli</TableCell>
-      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -10,8 +10,16 @@ import {
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export const Orders = () => {
+
+  const {data: result} = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -35,9 +43,9 @@ export const Orders = () => {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, index) => (
-                  <OrderTableRow key={index} />
-                ))}
+               {result && result.orders.map(order => (
+                 <OrderTableRow key={order.orderId} order={order} />
+               )) }
               </TableBody>
             </Table>
           </div>
