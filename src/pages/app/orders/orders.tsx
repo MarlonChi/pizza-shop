a89297412618c1d44cb27e0f454b0846371b744a
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-10 21:13:29 -0300

    feat: orders loading

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
+import { useQuery } from "@tanstack/react-query";
 
 import {
   Table,
@@ -12,8 +13,8 @@ import {
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
-import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export const Orders = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -27,7 +28,7 @@ export const Orders = () => {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? 1);
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -68,6 +69,8 @@ export const Orders = () => {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
+
                 {result &&
                   result.orders.map((order) => (
                     <OrderTableRow key={order.orderId} order={order} />
